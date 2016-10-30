cf4b3600a9a0b740da6da0fa4fb08cdc17090969
Jan Koppe <post@jankoppe.de>
2016-10-30 08:59:45 +0100

    cost calculation somewhat working - 8bit integer overflow galore.

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ int main( int argc, char** argv ) {
     return -1;
   }
 
-  std::cout << "Calculating energy function for image" << std::endl;
+  std::cout << "calculate energy function" << std::endl;
   cv::Mat energy = calcEnergy(image);
 
-  std:: cout << "Write image of energy function to energy.jpg" << std::endl;
-  cv::imwrite("energy.jpg", energy);
+  std::cout << "calculate cost matrix" << std::endl;
+  cv::Mat cost = calcCost(energy, HORI);
 
   return 0;
 }
 
 cv::Mat calcEnergy(cv::Mat input) {
-  cv::Mat output = cv::Mat(input.size(), input.type());
+  cv::Mat output = cv::Mat::zeros(input.size(), CV_8UC1);
   for(int x = 0; x < output.size().width; x++) {
     for(int y = 0; y < output.size().height; y++) {
       int xPrev = x;
@@ -37,6 +37,7 @@ cv::Mat calcEnergy(cv::Mat input) {
       if(y > 0) yPrev--;
       if(y < output.size().height) yNext++;
 
+      // Calculate gradient energy for a pixel as described on chapter 4, slide 11.
       int xEnergy =
         (input.at<cv::Vec3b>(xPrev, y).val[0] - input.at<cv::Vec3b>(xNext, y).val[0]) / 2 +
         (input.at<cv::Vec3b>(xPrev, y).val[1] - input.at<cv::Vec3b>(xNext, y).val[1]) / 2 +
@@ -46,10 +47,37 @@ cv::Mat calcEnergy(cv::Mat input) {
         (input.at<cv::Vec3b>(x, yPrev).val[1] - input.at<cv::Vec3b>(x, yPrev).val[1]) / 2 +
         (input.at<cv::Vec3b>(x, yPrev).val[2] - input.at<cv::Vec3b>(x, yPrev).val[2]) / 2 ;
 
+      // use absolute values because energy can be negative - uint "underflow"
+      // see avidan et al., section 3 for reference
       int energy = abs(xEnergy) + abs(yEnergy);
-
-      output.at<cv::Vec3b>(x, y) = cv::Vec3b(energy, energy, energy);
+      output.at<uchar>(x, y) = energy;
     }
   }
   return output;
+}
+
+cv::Mat calcCost(cv::Mat energy, int dir) {
+  // calculate path cost matrix
+  cv::Mat cost = cv::Mat::zeros(energy.size(), CV_32F);
+  if(dir == HORI) {
+    energy.col(0).copyTo(cost.col(0));
+    for (int x = 1; x < energy.size().width; x++) {
+      for (int y = 0; y < energy.size().height; y++) {
+        int above = y;
+        int below = y;
+        if (y > 0) above--;
+        if (y < energy.size().height) below++;
+
+        int pixelcost = std::min(std::min(cost.at<uchar>(x - 1, above), cost.at<uchar>(x - 1, y)),
+                            cost.at<uchar>(x - 1, below));
+        cost.at<uchar>(x,y) = pixelcost + energy.at<uchar>(x, y);
+      }
+    }
+  } else {
+    energy.row(0).copyTo(cost.row(0));
+  }
+  cv::Mat cost8bit;
+  cost.convertTo(cost8bit, CV_8UC1, 1);
+  cv::imwrite("cost.jpg", cost8bit);
+  return cost;
 }
--- main.hpp
+++ main.hpp
@@ -3,10 +3,15 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#define HORI 0
+#define VERT 1
 
 cv::Mat calcEnergy(cv::Mat input);
+cv::Mat calcCost(cv::Mat input, int dir);
+std::vector<int> findSeam(cv::Mat input, int dir);
 
 #endif
