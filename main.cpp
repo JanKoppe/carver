1a466bff75a1d15efdcd44aefac9155e488a0579
Jan Koppe <post@jankoppe.de>
2016-11-01 05:38:40 +0100

    bit of cleanup
    used namespace to declutter code, tried to remove some code
    redundancies.
    
    still having some problems with horizontal vs vertical code.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,54 @@
 #include "main.hpp"
 
+using namespace std;
+using namespace cv;
+
 int main( int argc, char** argv ) {
   if( argc != 5) {
-    std::cout << "Usage: " << argv[0] << " [inputimage] [x-Delta] [y-Delta] [output]" << std::endl;
+    cout << "Usage: " << argv[0] << " [inputimage] [x-Delta] [y-Delta] [output]" << endl;
     return -1;
   }
 
-  cv::Mat image;
-  image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-
+  Mat image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
   if(!image.data) {
-    std::cout << "Could not open or find the image" << std::endl;
+    cout << "Could not open or find the image" << endl;
     return -1;
   }
 
-  std::cout << "calculate energy function" << std::endl;
-  cv::Mat energy = calcEnergy(image);
-
-  cv::Mat jpeg;
-  energy.convertTo(jpeg, CV_8UC1, 0.5);
-  cv::imwrite("energy.jpg", jpeg);
-
-  std::cout << "calculate cost matrix" << std::endl;
-  cv::Mat cost = calcCost(energy, HORI);
-
-  cost.convertTo(jpeg, CV_8UC1, 0.2);
-  cv::imwrite("cost.jpg", jpeg);
-
-  std::vector<int> seam = findSeam(cost, HORI);
-
-  // debugging code: paint seam red and save image
-   std::vector<int>::iterator it = seam.begin();
-  for (int x = image.size().width; it != seam.end() && x > 0 ; ++it, x++) {
-    image.at<cv::Vec3b>(*it, x) = cv::Vec3b (0, 0, 255);
+  Mat jpeg;
+  int xdelta = atoi(argv[2]);
+  int ydelta = atoi(argv[3]);
+  while(xdelta + ydelta  > 0) {
+    cout << xdelta << " " << ydelta << endl;
+    Mat energy = calcEnergy(image);
+    energy.convertTo(jpeg, CV_8UC1, 0.5);
+    if(xdelta > 0) {
+      xdelta--;
+      Mat cost = calcCost(energy, VERT);
+      cost.convertTo(jpeg, CV_8UC1, 1.0/32.0);
+      imwrite("energy.jpg", jpeg);
+      vector<int> seam = findSeam(cost, VERT);
+      // debugging code: paint seam red and save image
+       vector<int>::iterator it = seam.begin();
+      for (int i = image.size().width; it != seam.end() && i > 0 ; ++it, i--) {
+        image.at<Vec3b>(i - 1, *it) = Vec3b (0, 0, 255);
+      }
+    }
+    if(ydelta > 0) {
+      ydelta--;
+      Mat cost = calcCost(energy, HORI);
+      cost.convertTo(jpeg, CV_8UC1, 1.0/32.0);
+      imwrite("energy.jpg", jpeg);
+      vector<int> seam = findSeam(cost, HORI);
+      // debugging code: paint seam red and save image
+       vector<int>::iterator it = seam.begin();
+      for (int i = image.size().width; it != seam.end() && i > 0 ; ++it, i--) {
+        image.at<Vec3b>(*it, i -1) = Vec3b (0, 0, 255);
+      }
+    }
   }
-  cv::imwrite(argv[4], image);
+  imwrite(argv[4], image);
+    imwrite("cost.jpg", jpeg);
   //
   return 0;
 }
@@ -45,29 +59,27 @@ int main( int argc, char** argv ) {
  * @param input Matrix to calculate the energy for.
  * @return Matrix containing the energy values for pixels in input.
  */
-cv::Mat calcEnergy(cv::Mat input) {
-  cv::Mat energy = cv::Mat::zeros(input.size(), CV_32FC1);
-  for(int x = 0; x < energy.size().width; x++) {
-    for(int y = 0; y < energy.size().height; y++) {
-      int xPrev = x;
-      int xNext = x;
-      int yPrev = y;
-      int yNext = y;
+Mat calcEnergy(Mat input) {
+  Mat energy = Mat::zeros(input.size(), CV_32FC1);
+  int xmax = energy.size().width;
+  int ymax = energy.size().height;
+  for(int x = 0; x < xmax; x++) {
+    for(int y = 0; y < ymax; y++) {
       // prevent attempts to access outside of image
-      if(x > 0) xPrev--;
-      if(x < energy.size().width) xNext++;
-      if(y > 0) yPrev--;
-      if(y < energy.size().height) yNext++;
+      int xprev = max(0, x - 1);
+      int xnext = min(xmax, x + 1);
+      int yprev = max(0, y - 1);
+      int ynext = min (ymax, y + 1);
 
       // Calculate gradient energy for a pixel as described on chapter 4, slide 11.
       int xEnergy =
-        (input.at<cv::Vec3b>(y, xPrev).val[0] - input.at<cv::Vec3b>(y, xNext).val[0]) / 2 +
-        (input.at<cv::Vec3b>(y, xPrev).val[1] - input.at<cv::Vec3b>(y, xNext).val[1]) / 2 +
-        (input.at<cv::Vec3b>(y, xPrev).val[2] - input.at<cv::Vec3b>(y, xNext).val[2]) / 2 ;
+        (input.at<Vec3b>(y, xprev).val[0] - input.at<Vec3b>(y, xnext).val[0]) / 2 +
+        (input.at<Vec3b>(y, xprev).val[1] - input.at<Vec3b>(y, xnext).val[1]) / 2 +
+        (input.at<Vec3b>(y, xprev).val[2] - input.at<Vec3b>(y, xnext).val[2]) / 2 ;
       int yEnergy =
-        (input.at<cv::Vec3b>(yPrev, x).val[0] - input.at<cv::Vec3b>(yPrev, x).val[0]) / 2 +
-        (input.at<cv::Vec3b>(yPrev, x).val[1] - input.at<cv::Vec3b>(yPrev, x).val[1]) / 2 +
-        (input.at<cv::Vec3b>(yPrev, x).val[2] - input.at<cv::Vec3b>(yPrev, x).val[2]) / 2 ;
+        (input.at<Vec3b>(yprev, x).val[0] - input.at<Vec3b>(ynext, x).val[0]) / 2 +
+        (input.at<Vec3b>(yprev, x).val[1] - input.at<Vec3b>(ynext, x).val[1]) / 2 +
+        (input.at<Vec3b>(yprev, x).val[2] - input.at<Vec3b>(ynext, x).val[2]) / 2 ;
 
       // use absolute values because energy can be negative
       // see avidan et al., section 3 for reference
@@ -83,37 +95,33 @@ cv::Mat calcEnergy(cv::Mat input) {
  * @param dir Sets horizontal (ltr) or vertical (ttb) direction.
  * @return Path cost matrix of same type as energy matrix.
  */
-cv::Mat calcCost(cv::Mat energy, int dir) {
-  cv::Mat cost = cv::Mat::zeros(energy.size(), energy.type());
+Mat calcCost(Mat energy, int dir) {
+  Mat cost = Mat::zeros(energy.size(), energy.type());
+  int imax, jmax, l, r;
   if(dir == HORI) {
     // cost for the first step is their own energy.
     energy.col(0).copyTo(cost.col(0));
-    for (int x = 1; x < energy.size().width; x++) {
-      for (int y = 0; y < energy.size().height; y++) {
-        int above = y;
-        int below = y;
-        // watch out for those borders.
-        if (y > 0) above--;
-        if (y < energy.size().height) below++;
-
-        float cheapest = std::min(std::min(cost.at<float>(above, x - 1), cost.at<float>(y, x - 1)),
-                            cost.at<float>(below, x - 1));
-        // cost is the total previous cost from the cheapest path so far and the pixels own energy.
-        cost.at<float>(y, x) = cheapest + energy.at<float>(y, x);
-      }
-    }
+    imax = energy.size().width;
+    jmax = energy.size().height;
   } else {
     energy.row(0).copyTo(cost.row(0));
-    for (int y = 1; y < energy.size().width; y++) {
-      for (int x = 0; x < energy.size().height; x++) {
-        int left = x;
-        int right = x;
-        if (x > 0) left--;
-        if (x < energy.size().width) right++;
-
-        float pixelcost = std::min(std::min(cost.at<float>(y - 1, left), cost.at<float>(y - 1, x)),
-                            cost.at<float>(y - 1, right));
-        cost.at<float>(y, x) = pixelcost + energy.at<float>(y, x);
+    imax = energy.size().height;
+    jmax = energy.size().width;
+  }
+  for (int i = 1; i < imax; i++) {
+    for (int j = 0; j < jmax; j++) {
+      // watch out for those borders.
+      l = max(0, j - 1);
+      r = min(jmax, j + 1);
+      if(dir == HORI) {
+        float cheapest = min(min(cost.at<float>(l, i - 1), cost.at<float>(j, i - 1)),
+                             cost.at<float>(r, i - 1));
+        // cost is the total previous cost from the cheapest path so far and the pixels own energy.
+        cost.at<float>(j, i) = cheapest + energy.at<float>(j, i);
+      } else {
+        float cheapest = min(min(cost.at<float>(i - 1, l), cost.at<float>(i - 1, j)),
+                             cost.at<float>(i - 1, r));
+        cost.at<float>(i, j) = cheapest + energy.at<float>(i, j);
       }
     }
   }
@@ -126,9 +134,9 @@ cv::Mat calcCost(cv::Mat energy, int dir) {
  * @param dir Direction of the cost matrix and seam
  * @return vector of seamcoordinates in respect to the direction
  */
-std::vector<int> findSeam(cv::Mat cost, int dir) {
-  std::vector<int> seam;
-  int min_pos;
+vector<int> findSeam(Mat cost, int dir) {
+  vector<int> seam;
+  int min_pos, imax, jmax;
   float min = FLT_MAX;
   if(dir == HORI) {
     seam.reserve(cost.size().width);
